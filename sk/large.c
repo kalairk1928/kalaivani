6da6ad2f9e87f432fe8fefb442116460f6cf0098
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-08 11:28:43 +0000

    Create large.c

---
+++ sk/large.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d",&a);
+scanf("%d",&b);
+scanf("%d",&c);
+if(a>b)
+{
+prinf("a is big");
+}
+ifelse
+{
+printf("b is big");
+}
+else
+printf("c is big");
+}
