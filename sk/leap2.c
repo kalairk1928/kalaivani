fe9927c57281c97c3496cffecfae654ca7ea160b
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-24 16:22:24 +0530

    Update leap2.c

--- sk/leap2.c
+++ sk/leap2.c
@@ -2,19 +2,19 @@
  
 void main()
 {
-  int year;
+  int y;
  
   printf("Enter a year to check if it is a leap year\n");
-  scanf("%d", &year);
+  scanf("%d", &y);
  
-  if ( year%400 == 0)
-    printf("%d is a leap year.\n", year);
-  else if ( year%100 == 0)
-    printf("%d is not a leap year.\n", year);
-  else if ( year%4 == 0 )
-    printf("%d is a leap year.\n", year);
+  if ( y%400 == 0)
+    printf("%d is a leap year.\n", y);
+  else if ( y%100 == 0)
+    printf("%d is not a leap year.\n", y);
+  else if ( y%4 == 0 )
+    printf("%d is a leap year.\n", y);
   else
-    printf("%d is not a leap year.\n", year);  
+    printf("%d is not a leap year.\n", y);  
  
   
 }
