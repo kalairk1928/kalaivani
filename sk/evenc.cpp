1ac676f3a754e3554ed799a6414ae97e28cbda05
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-24 16:55:54 +0530

    Create evenc.cpp

---
+++ sk/evenc.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int a;
+ int b;
+ cout<<" enter number "<<endl;
+ cin>>a;
+ cout<<" enter number "<<endl;
+ cin>>b;
+ cout<<"*********************************"<<endl;
+ cout<<"odd number"<<endl;
+ cout<<"**********************************"<<endl;
+ for(;a<=b;)
+ {
+ cout<<a<<endl;
+ a=a+2;
+ }
+}
