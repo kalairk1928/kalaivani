4ef6214dbf31fd7ca9c4dbd05ef880859cf12392
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-24 16:52:15 +0530

    Create oddc.cpp

---
+++ sk/oddc.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ 
+ int a;
+ int b;
+ cout<<" enter number "<<endl;
+ cin>>a;
+ cout<<" enter number "<<endl;
+ cin>>b;
+ cout<<"*********************************"<<endl;
+ cout<<"even number"<<endl;
+ cout<<"**********************************"<<endl;
+ for(;a<=b;)
+ {
+ cout<<a<<endl;
+ a=a+2;
+ }
+}
