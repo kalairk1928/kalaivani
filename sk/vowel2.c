87e0eb30c56fc096e05e8e8effc74e113b89d28a
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-24 16:08:47 +0530

    Update vowel2.c

--- sk/vowel2.c
+++ sk/vowel2.c
@@ -2,14 +2,14 @@
 
  
 void main(){
-    char c;
+    char k;
     printf("Enter a Character: ");
-    scanf("%c", &c);
-    if(c == 'a' || c == 'e' || c =='i' || c=='o' || c=='u' || c=='A'
-          || c=='E' || c=='I' || c=='O' || c=='U'){
-        printf("%c is a Vowel\n", c);
+    scanf("%c", &k);
+    if(k == 'a' || k == 'e' || k =='i' || k=='o' || k=='u' || k=='A'
+          || k=='E' || k=='I' || k=='O' || k=='U'){
+        printf("%c is a Vowel\n", k);
     } else {
-        printf("%c is a Consonant\n", c);
+        printf("%c is a Consonant\n", k);
     }
     
 }
