0dbbfd84eb55102c4037b803604cb7fdae857bc3
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-01-31 16:58:23 +0530

    Create big2.c

---
+++ sk/big2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,temp=0,n,j;
+    printf("enter a count:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+for(i=0;i<n;i++)
+{
+    for(j=1;j<n;j++)
+{
+    
+        if(a[i]>a[j])
+        {
+            temp=a[i];
+        }
+        else
+        {
+            temp=a[j];
+        }
+} 
+}
+printf("%d\n",temp);
+}
