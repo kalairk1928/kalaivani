c5152b01ef68c9e91228a4516fe18e4f4c41ba9d
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-02 14:38:07 +0530

    Create 26.c

---
+++ sk/26.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a[6],t=0,n,i,j;
+printf("enter a count:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}}}
+for(i=0;i<n;i++)
+{
+printf("the sorted array is:%d\n",a[i]);
+}
+}
