0b4551fe0be8af6f471c2a3c05d170eca1c3c28c
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-05 16:17:13 +0530

    Create 30.c

---
+++ sk/30.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int t,s,k;
+    printf("enter a times");
+    scanf("%d:%d\n%d:%d\n",&t,&s);
+    k=t-s;
+    printf("the result time is:%d\n",k);
+    
+}
+
+
