49997f2ae87db602716620f22ae9bea1a4045b5a
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-05 15:54:36 +0530

    Create 27.c

---
+++ sk/27.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int s;
+    printf("enter a string");
+    scanf("%d",&s);
+    if((s>0)&&(s<9))
+    {
+        printf("yes");
+        
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
+
