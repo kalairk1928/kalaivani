c372f42c66f2506a21601948ad436099720daa9e
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-09 13:53:49 +0530

    Create 41.c

---
+++ sk/41.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    char a[100]="kalaivani";
+    printf("how many time ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%s\n",a);
+    }
+}
