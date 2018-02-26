1b3ec03403d6134b8aeed6be70c7bb0f154c6fe9
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-26 15:45:36 +0530

    Create 61.c

---
+++ sk/61.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	char a[100];
+	int n,i,m;
+	scanf("%s",&a);
+	scanf("%d",&n);
+	m=strlen(a);
+	for(i=0;i<n;i++)
+	{
+		printf("%c",a[i]);
+	}
+}
