9c30128247bf274646a37b0c0d1e0ae2d4ac201a
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-28 16:08:28 +0530

    Create 78.c

---
+++ sk/78.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+   int k;
+   printf("enter a number:");
+   scanf("%d",&k);
+   if(k%13==0)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+}
