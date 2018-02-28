762064cabc47196d0db950acf043347b604729e6
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-02-28 16:32:22 +0530

    Create 76.c

---
+++ sk/76.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main()
+{
+int i,x,c=0;
+i=2;
+printf("enter a value:");
+scanf("%d",&x);
+while(i<x)
+{
+if(x%i==0)
+{
+c=c+1;
+}
+i++;
+}
+if(c==0)
+{
+printf("not composite");
+}
+else
+{
+printf("composite");
+}
+}
