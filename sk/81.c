6e15a2ea958d96cee379daef04e4acede6bb9a81
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-03-14 16:03:39 +0530

    Create 81.c

---
+++ sk/81.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int s,k,diff;
+scanf("%d",&s);
+scanf("%d",&k);
+diff=s-k;
+printf("%d",diff);
+if(s>k)
+{
+printf("go to fight");
+}
+else
+{
+printf("not go to fight");
+}
+}
