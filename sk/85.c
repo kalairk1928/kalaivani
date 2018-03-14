e003b93daccd7c2c3873d77198923d2212b14ce8
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-03-14 16:42:26 +0530

    Create 85.c

---
+++ sk/85.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+char str[100];
+printf("enter a string");
+scanf("%c",s);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]%2==0)
+{
+printf("%c",str[i]);
+}
+}
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]%2==1)
+{
+printf("%c",str[i]);
+}
+}
+}
