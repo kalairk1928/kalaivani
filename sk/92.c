e001b21c7b03ff59922ccbdb7a74b711b6b85582
kalairk1928 <31534416+kalairk1928@users.noreply.github.com>
2018-03-19 15:56:06 +0530

    Create 92.c

---
+++ sk/92.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a,b[5],k,c=0;
+printf("enter the total no of element");
+scanf("%d",&a);
+for(k=0;k<5;k++)
+{
+    scanf("%d",&b[i]);
+c=c+b[i];
+}
+printf("the sum of all numbers is %d",c);
+}
